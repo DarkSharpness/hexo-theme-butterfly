f583fa85874febe7136718587e151d5bd973e051
DarkSharpness <2040703891@qq.com>
2025-02-03 13:02:12 +0800

    feat: add a template for bilibili videos

--- scripts/tag/my_template.js
+++ scripts/tag/my_template.js
@@ -1,6 +1,9 @@
 /**
- * Photo
- * {% pixiv pid author url }
+ * 1. Photo
+ * {% pixiv pid author url %}
+ * 
+ * 2. bilibili
+ * {% bilibili ... %}
  */
 
 'use strict'
@@ -12,4 +15,32 @@ const photo_fn = args => {
     return result
 }
 
-hexo.extend.tag.register('pixiv', photo_fn, {ends: false})
+const bilibili_fn = args => {
+    let params = {};
+    args.forEach(arg => {
+        if (!arg.includes('=')) { // key only
+            params[arg.trim()] = true;
+        } else { // key-value pair
+            const [key, value] = arg.split('=');
+            params[key.trim()] = value.trim();
+        }
+    });
+
+    let result = `<div style="position: relative; padding: 30% 45%;"><iframe style="position: absolute; width: 100%; height: 100%; left: 0; top: 0;" src="https://player.bilibili.com/player.html?isOutside=true`
+
+    if (params.aid) result += `&aid=${params.aid}`
+    if (params.bvid) result += `&bvid=${params.bvid}`
+    if (params.cid) result += `&cid=${params.cid}`
+    if (params.p) result += `&p=${params.p}`
+    if (params.t) result += `&t=${params.t}`
+
+    result += `&muted=${params.muted ? 1 : 0}`
+    result += `&autoplay=${params.autoplay ? 1 : 0}`
+    result += `&danmaku=${params.danmaku ? 1 : 0}`
+
+    result += `&high_quality=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe></div>`
+    return result
+}
+
+hexo.extend.tag.register('pixiv', photo_fn, { ends: false })
+hexo.extend.tag.register('bilibili', bilibili_fn, { ends: false })
