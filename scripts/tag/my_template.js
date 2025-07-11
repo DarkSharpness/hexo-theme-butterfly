0a9c4babe30486531df9acd6ace6ebda3ea6281b
DarkSharpness <2040703891@qq.com>
2025-07-11 01:43:47 -0700

    feat: support <> wrapped url

--- scripts/tag/my_template.js
+++ scripts/tag/my_template.js
@@ -10,7 +10,8 @@
 
 const photo_fn = args => {
     const [pid, author, url] = args
-    let result = `<img src="${url}">`
+    const raw_url = url && url.startsWith('<') && url.endsWith('>') ? url.slice(1, -1) : url;
+    let result = `<img src="${raw_url}">`
     result += `<center><font color=grey><a href="https://www.pixiv.net/artworks/${pid}"> Artwork ${pid} by ${author} </a></font></center><br/>`
     return result
 }
